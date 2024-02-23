967a5ba9201bbf62ab6c362f9cb60727aef68d92
Seko461 <sekopierre@gmail.com>
2024-02-23 07:57:49 +0100

    ok iteration valid (yessss)

--- mail/assets/js/script.js
+++ mail/assets/js/script.js
@@ -14,35 +14,40 @@ let button = document.getElementById('submit_button');
 button.addEventListener('click', (e) => {
     e.preventDefault()
 
-    // recupero il valore dell'input
-    let email = document.getElementById('input').value;
 
-    // se non ho inserito nulla nel campo
-    if (email == '') {
-        alert("Devi indicare un indirizzo email"); return false;
-    }
+    var ok = ["seko@gmail.com", "alba@outlook.it"];
 
-    // verifico se è un indirizzo valido
-    let ok = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)]
+
+    // /^\w+([\.-] ?\w +) *@\w + ([\.-] ?\w +)* (\.\w{ 2, 3 }) +$ /.test(email), 
 
     for (i = 0; i < ok.length; i++) {
 
-        let valid = document.getElementById('valid');
-        if (email = ok[i]) {
+        // recupero il valore dell'input
+        let email = document.getElementById('input').value;
+
+        // se non ho inserito nulla nel campo
+        if (email == '') {
+            alert("Devi indicare un indirizzo email"); return false;
+        }
+        console.log(typeof email, email);
+        // verifico se è un indirizzo valido
 
 
+        var valid = document.getElementById('valid');
+        if (email == ok[i]) {
             var textNode = document.createTextNode("L'indirizzo email che hai inserito e' valido");
-
         }
         else {
             var textNode = document.createTextNode("L'indirizzo email che hai inserito non e' valido");
         }
+        valid.appendChild(textNode);
+        return true;
 
 
 
 
     }
-    valid.appendChild(textNode);
+
 
 
 
